960978a4dd33fc4595ae45d9e414069642c35e4e
Benjamin Seamons <Masterofart605@gmail.com>
2022-09-08 16:30:39 -0600

    finished step: 3, Test Limits

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿//WorkWithIntegers();
-OrderPrecedence();
+//OrderPrecedence();
+//testLimits();
+
+
+
 
 void WorkWithIntegers()
 {
@@ -37,4 +41,21 @@ int f = 4;
 int g = 3;
 int h = (e+f)/g;
 Console.WriteLine(h);
+}
+
+void testLimits(){
+int a =7;
+int b = 4;
+int c = 3;
+int d = (a + b ) / c;
+int e = (a + b ) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
 }
