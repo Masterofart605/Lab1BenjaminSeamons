0acc41b00300d9b165f7c9c260d2a4c7cc557cd0
Benjamin Seamons <Masterofart605@gmail.com>
2022-09-08 16:21:37 -0600

    finished step: 2, Order Precedence

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿//Addition
+﻿//WorkWithIntegers();
+OrderPrecedence();
+
+void WorkWithIntegers()
+{
+//Addition
 int a = 18;
 int b = 6;
 int c = a + b;
@@ -15,3 +20,21 @@ Console.WriteLine(c);
 //Division
 c = a / b;
 Console.WriteLine(c);
+
+//Random stuff
+c = a+b/a-b*a;
+Console.WriteLine(c);
+}
+void OrderPrecedence(){
+    int a = 5;
+int b = 4;
+int c = 2;
+int d = (a + b) * c + a * (b + c);
+Console.WriteLine(d);
+
+int e = 7;
+int f = 4;
+int g = 3;
+int h = (e+f)/g;
+Console.WriteLine(h);
+}
